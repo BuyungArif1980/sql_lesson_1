a08612b6032a473f05ee1a6bfabf23d347f7d2fb
yungbow <buyungarif.budpar@gmail.com>
2020-05-06 06:17:19 +0700

    Lesson 8. LIKE (2)

--- exercise2.sql
+++ exercise2.sql
@@ -1,8 +1,8 @@
 /*
-dibawah "FROM purchases" tambahkan code untuk mengambil baris dengan
-nilai tanggal "2018-11-01" dan sebelumnya dari kolom "purchased_at"
+dibawah "FROM purchases" tambahkan code untuk
+mengambil baris dimana nilai "name" diakhiri dengan kata "puding"
 */
 
 SELECT *
 FROM purchases
-WHERE purchased_at <= '2018-11-01';
+WHERE name LIKE '%puding';
